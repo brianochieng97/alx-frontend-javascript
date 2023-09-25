e1af1628fe20e00d6d1c3e97b24c9b1e30205f85
brianochieng97 <brianochieng9725@gmail.com>
2023-09-25 16:32:25 +0000

    Block scope

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,18 @@
+const function taskFirst() {
+	var = 'I Prefer const when I can,';
+	return task;
+}
+
+let function getLast() {
+	return ' is okay';
+}
+
+const function taskNext() {
+	var combination = 'But sometimes let';
+	combination += getLast();
+	return combination;
+}
+
+
+console.log(`${taskFirst()} ${taskNext()}`);
+
